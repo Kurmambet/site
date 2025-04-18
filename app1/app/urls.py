a32aba37ef9908a91b2000eece84f7cae0de9ce3
Petr <pitr.vishnyakovv.05@gmail.com>
2025-04-18 16:11:31 +0500

    index and about

--- app1/app/urls.py
+++ app1/app/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import include, path
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
+    path('', include('main.urls', namespace='main'))
 ]
--- app1/main/views.py
+++ app1/main/views.py
@@ -5,11 +5,17 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title' : 'Home',
+        'title' : 'Home - главная',
         'content' : 'Главная страница сайта',
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('<h1>about page</h1>')
+    context = {
+        'title': 'HOME - about',
+        'content' : 'Страница о нас',
+        'text_on_page': 'some pafos text'
+
+    }
+    return render(request, 'main/about.html', context)
