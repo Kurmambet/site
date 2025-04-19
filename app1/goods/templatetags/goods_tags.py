5df42e943e36cff8a8cf7381c2def58cc43ff802
Petr <pitr.vishnyakovv.05@gmail.com>
2025-04-19 22:38:09 +0500

    8.00.00 Q class

--- app1/goods/templatetags/goods_tags.py
+++ app1/goods/templatetags/goods_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from goods.models import Categories, Products
+from django.utils.http import urlencode
+from goods.models import Categories
 
 register = template.Library()
 
@@ -8,6 +9,9 @@ register = template.Library()
 def tag_categories():
     return Categories.objects.all()
 
-# @register.simple_tag()
-# def tag_products():
-#     return Products.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
