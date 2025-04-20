63bf9f8987e4d6bde129496f5b590b5854ad4475
Petr <pitr.vishnyakovv.05@gmail.com>
2025-04-20 14:57:14 +0500

    full search done

--- app1/goods/utils.py
+++ app1/goods/utils.py
@@ -1,13 +1,83 @@
-from django.db.models import Q
+# from django.db.models import Q
 from goods.models import Products
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
-    for token in keywords:
-        q_objects |= Q
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name", query, start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description", query, start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result   
+#                               /catalog 
+#                                           {% autoescape off %}
+#                                               {% if product.headline %}
+#                                                   {{ product.headline }}
+#                                               {% else %}
+#                                                   {{ product.name }}
+#                                               {% endif %}
+#                                           {% endautoescape %}
+#
+#                                           {% autoescape off %}
+#                                               {% if product.bodyline %}
+#                                                   {{ product.bodyline|truncatechars:100 }}
+#                                               {% else %}
+#                                                   {{ product.description|truncatechars:35 }}
+#                                               {% endif %}
+#                                           {% endautoescape %}
+#
+#
+#
+#
+#
+    # vector = SearchVector("name", "description")
+    # query = SearchQuery(query)
+    # return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+#
+#
+#
+#
+    # return Products.objects.annotate(search=SearchVector("name", "description")).filter(search=query)
+#
+#
+#
+#
+    # return Products.objects.filter(description__search=query)
+#
+#
+#
+#
+    # keywords = [word for word in query.split() if len(word) > 2]
+
+    # q_objects = Q()
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Products.objects.filter(q_objects)
