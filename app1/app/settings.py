5f7487435ad4baf7994a4d1383eca21a9baeea34
Petr <pitr.vishnyakovv.05@gmail.com>
2025-04-22 21:40:56 +0500

    cart tamplates included

--- app1/app/settings.py
+++ app1/app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'main',
     'goods',
     'users',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -156,3 +157,5 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_URL = '/user/login/'
