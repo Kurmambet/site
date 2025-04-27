8f3749081dd5c846872beb9ce9de235884219c8e
Petr <pitr.vishnyakovv.05@gmail.com>
2025-04-27 17:12:54 +0500

    validator phonenumber

--- app1/orders/forms.py
+++ app1/orders/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 class CreateOrderForm(forms.Form):
@@ -15,6 +16,18 @@ class CreateOrderForm(forms.Form):
             ("0", False),
             ("1", True),],
         )
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number'] 
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+        
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError('Неверный формат номера')
+        
+        return data
 
 
 
